6c303ec5b6dcdf9d96247c28b6b1ca515ad6c07e
zcc <listenzcc@mail.bnu.edu.cn>
2019-08-20 09:14:53 +0800

    add WLS and IPython profiles

--- mk_profiles_json.py
+++ mk_profiles_json.py
@@ -9,16 +9,19 @@ import random
 
 # Read template json file
 with open('template.json') as f:
-    profile = json.load(f)
+    profiles = json.load(f)
 
 # Set random wall paper
 wall_paper_path = 'C:\\Users\\liste\\Pictures\\WallPapers'
-profile['profiles'][0]['backgroundImage'] = os.path.join(
-    wall_paper_path, random.choice(os.listdir(wall_paper_path)))
+
+for j in range(len(profiles['profiles'])):
+    profiles['profiles'][j]['backgroundImage'] = os.path.join(
+        wall_paper_path, random.choice(os.listdir(wall_paper_path)))
+    profiles['profiles'][j]['backgroundImageOpacity'] = 0.2
 
 # Unset startingDirectory
-profile['profiles'][0].pop('startingDirectory')
+profiles['profiles'][0].pop('startingDirectory')
 
 # Write profiles json file
 with open('profiles.json', 'w') as f:
-    json.dump(profile, f)
+    json.dump(profiles, f)
